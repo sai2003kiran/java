170714710f3ca4e67903f20e03dd64793bb69c75
Sai Kiran M <saik03414@gmail.com>
2025-03-24 20:58:14 +0530

    one type of constructor using parameter

--- TubeLight.java
+++ TubeLight.java
@@ -2,10 +2,10 @@ class TubeLight {
 int tubeLength; 
 int tubePower; 
 String tubeColor;
-public TubeLight() {
+TubeLight() {
 System.out.println("TubeLight Zero constructor is invoked");
 }
-public TubeLight(int length, int power, String color) {
+TubeLight(int length, int power, String color) {
 System.out.println("TubeLight para constructor is invoked");
 	this.tubeLength = length;
 	this.tubePower = power;
