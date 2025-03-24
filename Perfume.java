dd65c5b1c0bfae229f091b359a695b6403674706
Sai Kiran M <saik03414@gmail.com>
2025-03-24 20:48:45 +0530

    Perfume.java

--- Perfume.java
+++ Perfume.java
@@ -4,13 +4,13 @@ class Perfume{
     int perfumeLongevity; // in hours
 
     // Constructor without arguments
-    public Perfume() {
-				System.out.println("perfume zero constructor is invoked");
+    Perfume() {
+       System.out.println("perfume zero constructor is invoked");
     }
 
     // Constructor with arguments
-    public Perfume(String name, String fragrance, int longevity) {
-						System.out.println("perfume para constructor is invoked");
+    Perfume(String perfumeName, String perfumeFragrance, int perfumeLongevity) {
+        System.out.println("perfume para constructor is invoked");
         this.perfumeName = perfumeName;
         this.perfumeFragrance = perfumeFragrance;
         this.perfumeLongevity = perfumeLongevity;
