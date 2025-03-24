ff5bd9f4c6a44b416fe0063c8aac16db75964438
Sai Kiran M <saik03414@gmail.com>
2025-03-24 21:03:45 +0530

    one more type of constructor using parameter

--- Perfume.java
+++ Perfume.java
@@ -7,8 +7,7 @@ class Perfume{
     Perfume() {
        System.out.println("perfume zero constructor is invoked");
     }
-
-    // Constructor with arguments
+   // Constructor with arguments
     Perfume(String perfumeName, String perfumeFragrance, int perfumeLongevity) {
         System.out.println("perfume para constructor is invoked");
         this.perfumeName = perfumeName;
