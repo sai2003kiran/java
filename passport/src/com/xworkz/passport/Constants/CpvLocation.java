1655aa57e39ea6c01f977b7e92b324558dccec7d
Sai Kiran M <saik03414@gmail.com>
2025-04-11 21:16:12 +0530

    Delete passport directory

--- passport/src/com/xworkz/passport/Constants/CpvLocation.java
+++
@@ -1,5 +0,0 @@
-package com.xworkz.passport.Constants;
-
-public enum CpvLocation {
-    Delhi, Passportoffice
-}
--- passport/src/com/xworkz/passport/Constants/DcdrLocation.java
+++
@@ -1,5 +0,0 @@
-package com.xworkz.passport.Constants;
-
-public enum DcdrLocation {
-    Bangalore,Mysore,Chennai
-}
